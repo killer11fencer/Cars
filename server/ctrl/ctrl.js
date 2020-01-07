5107a410523c8adcffc428e59b16776ac995f4d2
killer11fencer <jeoff.howe@gmail.com>
2020-01-07 16:36:39 -0700

    added ctrl functions

--- server/ctrl/ctrl.js
+++ server/ctrl/ctrl.js
@@ -1,7 +1,13 @@
 module.exports = {
     get_new: (req,res) => {
-        const db = =req.app.get('db')
+        const db = req.app.get('db')
         db.get_new().then(result=>res.status(200).send(result))
         .catch(err=>console.log('err on new listing',err))
+    },
+    get_sold: (req,res) => {
+        const db = req.app.get('db')
+        db.get_sold().then(result=>res.status(200).send(result))
+        .catch(err=>console.log('err on sold listing',err))
+
     }
 }

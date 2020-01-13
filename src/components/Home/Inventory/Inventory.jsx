5ee974b12e723c53e67bc40955203de64fd05aaa
killer11fencer <jeoff.howe@gmail.com>
2020-01-13 16:35:57 -0700

    added SQL annd new component

---
+++ src/components/Home/Inventory/Inventory.jsx
@@ -0,0 +1,12 @@
+import React, {Component} from 'react'
+import axios from 'axios'
+
+class Inventory extends Component {
+    constructor() {
+        super();
+        this.state = {
+            inventory: [],
+            verbage: {}
+        }
+    }
+}

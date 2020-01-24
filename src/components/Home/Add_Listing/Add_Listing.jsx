5fab61d4ba8fdf38c44fa403bfd5c995250d26fd
killer11fencer <jeoff.howe@gmail.com>
2020-01-24 14:45:37 -0700

    added vehicle api

---
+++ src/components/Home/Add_Listing/Add_Listing.jsx
@@ -0,0 +1,16 @@
+import React, {Component} from 'react'
+import axios from 'axios'
+
+class Add_Listing extends Component {
+    constructor() {
+        super();
+        this.state = {
+            year: [],
+            make: [],
+            model: []
+        }
+    }
+    componentDidMount () {
+        axios.get('https://vpic.nhtsa.dot.gov/api/vehicles/GetAllMakes').then(res => this.setState({make: res.data}))
+    }
+}
